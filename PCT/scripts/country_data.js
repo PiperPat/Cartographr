abd3f5f93590489ccec83921aab2361e0cdfb4d2
AntonHuggard <antonhuggard@gmail.com>
2021-06-28 12:07:35 +1200

    working on country toggle

---
+++ PCT/scripts/country_data.js
@@ -0,0 +1,919 @@
+
+const Central_African_Republic = {
+    code: "CF",
+    name: "Central African Republic",
+    joined: "24-Jan-78" 
+};
+
+const Congo = {
+    code: "CG",
+    name: "Congo",
+    joined: "24-Jan-78" 
+};
+
+const Switzerland = {
+    code: "CH",
+    name: "Switzerland",
+    joined: "24-Jan-78" 
+};
+
+const Cameroon = {
+    code: "CM",
+    name: "Cameroon",
+    joined: "24-Jan-78" 
+};
+
+const Germany = {
+    code: "DE",
+    name: "Germany",
+    joined: "24-Jan-78" 
+};
+
+const Gabon = {
+    code: "GA",
+    name: "Gabon",
+    joined: "24-Jan-78" 
+};
+
+const United_Kingdom = {
+    code: "GB",
+    name: "United Kingdom",
+    joined: "24-Jan-78" 
+};
+
+const Madagascar = {
+    code: "MG",
+    name: "Madagascar",
+    joined: "24-Jan-78" 
+};
+
+const Malawi = {
+    code: "MW",
+    name: "Malawi",
+    joined: "24-Jan-78" 
+};
+
+const Senegal = {
+    code: "SN",
+    name: "Senegal",
+    joined: "24-Jan-78" 
+};
+
+const Chad = {
+    code: "TD",
+    name: "Chad",
+    joined: "24-Jan-78" 
+};
+
+const Togo = {
+    code: "TG",
+    name: "Togo",
+    joined: "24-Jan-78" 
+};
+
+const USA = {
+    code: "US",
+    name: "United States of America",
+    joined: "24-Jan-78" 
+};
+
+const France = {
+    code: "FR",
+    name: "France",
+    joined: "25-Feb-78" 
+};
+
+const Brazil = {
+    code: "BR",
+    name: "Brazil",
+    joined: "9-Apr-78" 
+};
+
+const Luxembourg = {
+    code: "LU",
+    name: "Luxembourg",
+    joined: "30-Apr-78" 
+};
+
+const Sweden = {
+    code: "SE",
+    name: "Sweden",
+    joined: "17-May-78" 
+};
+
+const Japan = {
+    code: "JP",
+    name: "Japan",
+    joined: "1-Oct-78" 
+};
+
+const Denmark = {
+    code: "DK",
+    name: "Denmark",
+    joined: "1-Dec-78" 
+};
+
+const Austria = {
+    code: "AT",
+    name: "Austria",
+    joined: "23-Apr-79" 
+};
+
+const Monaco = {
+    code: "MC",
+    name: "Monaco",
+    joined: "22-Jun-79" 
+};
+
+const Netherlands = {
+    code: "NL",
+    name: "Netherlands",
+    joined: "10-Jul-79" 
+};
+
+const Romania = {
+    code: "RO",
+    name: "Romania",
+    joined: "23-Jul-79" 
+};
+
+const Norway = {
+    code: "NO",
+    name: "Norway",
+    joined: "1-Jan-80" 
+};
+
+const Liechtenstein = {
+    code: "LI",
+    name: "Liechtenstein",
+    joined: "19-Mar-80" 
+};
+
+const Australia = {
+    code: "AU",
+    name: "Australia",
+    joined: "31-Mar-80" 
+};
+
+const Hungary = {
+    code: "HU",
+    name: "Hungary",
+    joined: "27-Jun-80" 
+};
+
+const North_Korea = {
+    code: "KP",
+    name: "Democratic People's Republic of Korea",
+    joined: "8-Jul-80" 
+};
+
+const Finland = {
+    code: "FI",
+    name: "Finland",
+    joined: "1-Oct-80" 
+};
+
+const Belgium = {
+    code: "BE",
+    name: "Belgium",
+    joined: "14-Dec-81" 
+};
+
+const Sri_Lanka = {
+    code: "LK",
+    name: "Sri Lanka",
+    joined: "26-Feb-82" 
+};
+
+const Mauritania = {
+    code: "MR",
+    name: "Mauritania",
+    joined: "13-Apr-83" 
+};
+
+const Sudan = {
+    code: "SD",
+    name: "Sudan",
+    joined: "16-Apr-84" 
+};
+
+const Bulgaria = {
+    code: "BG",
+    name: "Bulgaria",
+    joined: "21-May-84" 
+};
+
+const South_Korea = {
+    code: "KR",
+    name: "Republic of Korea",
+    joined: "10-Aug-84" 
+};
+
+const Mali = {
+    code: "ML",
+    name: "Mali",
+    joined: "19-Oct-84" 
+};
+
+const Barbados = {
+    code: "BB",
+    name: "Barbados",
+    joined: "12-Mar-85" 
+};
+
+const Italy = {
+    code: "IT",
+    name: "Italy",
+    joined: "28-Mar-85" 
+};
+
+const Benin = {
+    code: "BJ",
+    name: "Benin",
+    joined: "26-Feb-87" 
+};
+
+const Burkina_Faso = {
+    code: "BF",
+    name: "Burkina Faso",
+    joined: "21-Mar-89" 
+};
+
+const Spain = {
+    code: "ES",
+    name: "Spain",
+    joined: "16-Nov-89" 
+};
+
+const Canada = {
+    code: "CA",
+    name: "Canada",
+    joined: "2-Jan-90" 
+};
+
+const Greece = {
+    code: "GR",
+    name: "Greece",
+    joined: "9-Oct-90" 
+};
+
+const Poland = {
+    code: "PL",
+    name: "Poland",
+    joined: "25-Dec-90" 
+};
+
+const Ivory_coast = {
+    code: "CI",
+    name: "Cote d'Ivoire",
+    joined: "30-Apr-91" 
+};
+
+const Guinea = {
+    code: "GN",
+    name: "Guinea",
+    joined: "27-May-91" 
+};
+
+const Mongolia = {
+    code: "MN",
+    name: "Mongolia",
+    joined: "27-May-91" 
+};
+
+const Armenia = {
+    code: "AM",
+    name: "Armenia",
+    joined: "25-Dec-91" 
+};
+
+const Belarus = {
+    code: "BY",
+    name: "Belarus",
+    joined: "25-Dec-91" 
+};
+
+const Georgia = {
+    code: "GE",
+    name: "Georgia",
+    joined: "25-Dec-91" 
+};
+
+const Kyrgyzstan = {
+    code: "KG",
+    name: "Kyrgyzstan",
+    joined: "25-Dec-91" 
+};
+
+const Kazakhstan = {
+    code: "KZ",
+    name: "Kazakhstan",
+    joined: "25-Dec-91" 
+};
+
+const Moldova = {
+    code: "MD",
+    name: "Republic of Moldova",
+    joined: "25-Dec-91" 
+};
+
+const Tajikistan = {
+    code: "TJ",
+    name: "Tajikistan",
+    joined: "25-Dec-91" 
+};
+
+const Turkmenistan = {
+    code: "TM",
+    name: "Turkmenistan",
+    joined: "25-Dec-91" 
+};
+
+const Ukraine = {
+    code: "UA",
+    name: "Ukraine",
+    joined: "25-Dec-91" 
+};
+
+const Uzbekistan = {
+    code: "UZ",
+    name: "Uzbekistan",
+    joined: "25-Dec-91" 
+};
+
+const Ireland = {
+    code: "IE",
+    name: "Ireland",
+    joined: "1-Aug-92" 
+};
+
+const Portugal = {
+    code: "PT",
+    name: "Portugal",
+    joined: "24-Nov-92" 
+};
+
+const New_Zealand = {
+    code: "NZ",
+    name: "New Zealand",
+    joined: "1-Dec-92" 
+};
+
+const Czechia = {
+    code: "CZ",
+    name: "Czechia",
+    joined: "1-Jan-93" 
+};
+
+const Slovakia = {
+    code: "SK",
+    name: "Slovakia",
+    joined: "1-Jan-93" 
+};
+
+const Viet_Nam = {
+    code: "VN",
+    name: "Viet Nam",
+    joined: "10-Mar-93" 
+};
+
+const Niger = {
+    code: "NE",
+    name: "Niger",
+    joined: "21-Mar-93" 
+};
+
+const Latvia = {
+    code: "LV",
+    name: "Latvia",
+    joined: "7-Sep-93" 
+};
+
+const China = {
+    code: "CN",
+    name: "China",
+    joined: "1-Jan-94" 
+};
+
+const Slovenia = {
+    code: "SI",
+    name: "Slovenia",
+    joined: "1-Mar-94" 
+};
+
+const Trinidad_Tobago = {
+    code: "TT",
+    name: "Trinidad and Tobago",
+    joined: "10-Mar-94" 
+};
+
+const Kenya = {
+    code: "KE",
+    name: "Kenya",
+    joined: "8-Jun-94" 
+};
+
+const Lithuania = {
+    code: "LT",
+    name: "Lithuania",
+    joined: "5-Jul-94" 
+};
+
+const Estonia = {
+    code: "EE",
+    name: "Estonia",
+    joined: "24-Aug-94" 
+};
+
+const Liberia = {
+    code: "LR",
+    name: "Liberia",
+    joined: "27-Aug-94" 
+};
+
+const Eswatini = {
+    code: "SZ",
+    name: "Eswatini",
+    joined: "20-Sep-94" 
+};
+
+const Mexico = {
+    code: "MX",
+    name: "Mexico",
+    joined: "1-Jan-95" 
+};
+
+const Uganda = {
+    code: "UG",
+    name: "Uganda",
+    joined: "9-Feb-95" 
+};
+
+const Singapore = {
+    code: "SG",
+    name: "Singapore",
+    joined: "23-Feb-95" 
+};
+
+const Iceland = {
+    code: "IS",
+    name: "Iceland",
+    joined: "23-Mar-95" 
+};
+
+const North_Macedonia = {
+    code: "MK",
+    name: "North Macedonia",
+    joined: "10-Aug-95" 
+};
+
+const Albania = {
+    code: "AL",
+    name: "Albania",
+    joined: "4-Oct-95" 
+};
+
+const Lesotho = {
+    code: "LS",
+    name: "Lesotho",
+    joined: "21-Oct-95" 
+};
+
+const Azerbaijan = {
+    code: "AZ",
+    name: "Azerbaijan",
+    joined: "25-Dec-95" 
+};
+
+const Turkey = {
+    code: "TR",
+    name: "Turkey",
+    joined: "1-Jan-96" 
+};
+
+const Israel = {
+    code: "IL",
+    name: "Israel",
+    joined: "1-Jun-96" 
+};
+
+const Cuba = {
+    code: "CU",
+    name: "Cuba",
+    joined: "16-Jul-96" 
+};
+
+const Saint_Lucia = {
+    code: "LC",
+    name: "Saint Lucia",
+    joined: "30-Aug-96" 
+};
+
+const Bosnia_Herzegovina = {
+    code: "BA",
+    name: "Bosnia and Herzegovina",
+    joined: "7-Sep-96" 
+};
+
+const Serbia = {
+    code: "RS",
+    name: "Serbia",
+    joined: "1-Feb-97" 
+};
+
+const Ghana = {
+    code: "GH",
+    name: "Ghana",
+    joined: "26-Feb-97" 
+};
+
+const Zimbabwe = {
+    code: "ZW",
+    name: "Zimbabwe",
+    joined: "11-Jun-97" 
+};
+
+const Sierra_Leone = {
+    code: "SL",
+    name: "Sierra Leone",
+    joined: "17-Jun-97" 
+};
+
+const Indonesia = {
+    code: "ID",
+    name: "Indonesia",
+    joined: "5-Sep-97" 
+};
+
+const Gambia = {
+    code: "GM",
+    name: "Gambia",
+    joined: "9-Dec-97" 
+};
+
+const Guinea_Bissau = {
+    code: "GW",
+    name: "Guinea-Bissau",
+    joined: "12-Dec-97" 
+};
+
+const Cyprus = {
+    code: "CY",
+    name: "Cyprus",
+    joined: "1-Apr-98" 
+};
+
+const Croatia = {
+    code: "HR",
+    name: "Croatia",
+    joined: "1-Jul-98" 
+};
+
+const Grenada = {
+    code: "GD",
+    name: "Grenada",
+    joined: "22-Sep-98" 
+};
+
+const India = {
+    code: "IN",
+    name: "India",
+    joined: "7-Dec-98" 
+};
+
+const UAE = {
+    code: "AE",
+    name: "United Arab Emirates",
+    joined: "10-Mar-99" 
+};
+
+const South_Africa = {
+    code: "ZA",
+    name: "South Africa",
+    joined: "16-Mar-99" 
+};
+
+const Costa_Rica = {
+    code: "CR",
+    name: "Costa Rica",
+    joined: "3-Aug-99" 
+};
+
+const Dominica = {
+    code: "DM",
+    name: "Dominica",
+    joined: "7-Aug-99" 
+};
+
+const Tanzania = {
+    code: "TZ",
+    name: "United Republic of Tanzania",
+    joined: "14-Sep-99" 
+};
+
+const Morocco = {
+    code: "MA",
+    name: "Morocco",
+    joined: "8-Oct-99" 
+};
+
+const Algeria = {
+    code: "DZ",
+    name: "Algeria",
+    joined: "8-Mar-00" 
+};
+
+const Antigua_Barbuda = {
+    code: "AG",
+    name: "Antigua and Barbuda",
+    joined: "17-Mar-00" 
+};
+
+const Mozambique = {
+    code: "MZ",
+    name: "Mozambique",
+    joined: "18-May-00" 
+};
+
+const Belize = {
+    code: "BZ",
+    name: "Belize",
+    joined: "17-Jun-00" 
+};
+
+const Colombia = {
+    code: "CO",
+    name: "Colombia",
+    joined: "28-Feb-01" 
+};
+
+const Ecuador = {
+    code: "EC",
+    name: "Ecuador",
+    joined: "7-May-01" 
+};
+
+const Equatorial_Guinea = {
+    code: "GQ",
+    name: "Equatorial Guinea",
+    joined: "17-Jul-01" 
+};
+
+const Philippines = {
+    code: "PH",
+    name: "Philippines",
+    joined: "17-Aug-01" 
+};
+
+const Oman = {
+    code: "OM",
+    name: "Oman",
+    joined: "26-Oct-01" 
+};
+
+const Zambia = {
+    code: "ZM",
+    name: "Zambia",
+    joined: "15-Nov-01" 
+};
+
+const Tunisia = {
+    code: "TN",
+    name: "Tunisia",
+    joined: "10-Dec-01" 
+};
+
+const Seychelles = {
+    code: "SC",
+    name: "Seychelles",
+    joined: "7-Nov-02" 
+};
+
+const Nicaragua = {
+    code: "NI",
+    name: "Nicaragua",
+    joined: "6-Mar-03" 
+};
+
+const Papua_New_Guinea = {
+    code: "PG",
+    name: "Papua New Guinea",
+    joined: "14-Jun-03" 
+};
+
+const Syria = {
+    code: "SY",
+    name: "Syrian Arab Republic",
+    joined: "26-Jun-03" 
+};
+
+const Egypt = {
+    code: "EG",
+    name: "Egypt",
+    joined: "6-Sep-03" 
+};
+
+const Botswana = {
+    code: "BW",
+    name: "Botswana",
+    joined: "30-Oct-03" 
+};
+
+const Namibia = {
+    code: "NA",
+    name: "Namibia",
+    joined: "1-Jan-04" 
+};
+
+const San_Marino = {
+    code: "SM",
+    name: "San Marino",
+    joined: "14-Dec-04" 
+};
+
+const Comoros = {
+    code: "KM",
+    name: "Comoros",
+    joined: "3-Apr-05" 
+};
+
+const Nigeria = {
+    code: "NG",
+    name: "Nigeria",
+    joined: "8-May-05" 
+};
+
+const Libya = {
+    code: "LY",
+    name: "Libya",
+    joined: "15-Sep-05" 
+};
+
+const SKN = {
+    code: "KN",
+    name: "Saint Kitts and Nevis",
+    joined: "27-Oct-05" 
+};
+
+const Montenegro = {
+    code: "ME",
+    name: "Montenegro",
+    joined: "3-Jun-06" 
+};
+
+const Laos = {
+    code: "LA",
+    name: "Lao People's Democratic Republic",
+    joined: "14-Jun-06" 
+};
+
+const Honduras = {
+    code: "HN",
+    name: "Honduras",
+    joined: "20-Jun-06" 
+};
+
+const Malaysia = {
+    code: "MY",
+    name: "Malaysia",
+    joined: "16-Aug-06" 
+};
+
+const El_Salvador = {
+    code: "SV",
+    name: "El Salvador",
+    joined: "17-Aug-06" 
+};
+
+const Guatemala = {
+    code: "GT",
+    name: "Guatemala",
+    joined: "14-Oct-06" 
+};
+
+const Malta = {
+    code: "MT",
+    name: "Malta",
+    joined: "1-Mar-07" 
+};
+
+const Bahrain = {
+    code: "BH",
+    name: "Bahrain",
+    joined: "18-Mar-07" 
+};
+
+const Dominican_Republic = {
+    code: "DO",
+    name: "Dominican Republic",
+    joined: "28-May-07" 
+};
+
+const Angola = {
+    code: "AO",
+    name: "Angola",
+    joined: "27-Dec-07" 
+};
+
+const STP = {
+    code: "ST",
+    name: "Sao Tome and Principe",
+    joined: "3-Jul-08" 
+};
+
+const Chile = {
+    code: "CL",
+    name: "Chile",
+    joined: "2-Jun-09" 
+};
+
+const Peru = {
+    code: "PE",
+    name: "Peru",
+    joined: "6-Jun-09" 
+};
+
+const Thailand = {
+    code: "TH",
+    name: "Thailand",
+    joined: "24-Dec-09" 
+};
+
+const Qatar   = {
+    code: "QA",
+    name: "Qatar  ",
+    joined: "3-Aug-11" 
+};
+
+const Rwanda = {
+    code: "RW",
+    name: "Rwanda",
+    joined: "31-Aug-11" 
+};
+
+const Brunei_Darussalam = {
+    code: "BN",
+    name: "Brunei Darussalam",
+    joined: "24-Jul-12" 
+};
+
+const Saudi_Arabia = {
+    code: "SA",
+    name: "Saudi Arabia",
+    joined: "3-Aug-13" 
+};
+
+const Iran = {
+    code: "IR",
+    name: "Iran (Islamic Republic of)",
+    joined: "4-Oct-13" 
+};
+
+const Kuwait = {
+    code: "KW",
+    name: "Kuwait",
+    joined: "9-Sep-16" 
+};
+
+const Djibouti = {
+    code: "DJ",
+    name: "Djibouti",
+    joined: "23-Sep-16" 
+};
+
+const Cambodia = {
+    code: "KH",
+    name: "Cambodia",
+    joined: "8-Dec-16" 
+};
+
+const Jordan = {
+    code: "JO",
+    name: "Jordan",
+    joined: "9-Jun-17" 
+};
+
+const Samoa = {
+    code: "WS",
+    name: "Samoa",
+    joined: "2-Jan-20" 
+};
+
+const SVG = {
+    code: "VC",
+    name: "Saint Vincent and the Grenadines",
+    joined: " 6 August 2002" 
+};
+
+const Russia = {
+    code: "RU",
+    name: "Russian Federation",
+    joined: "29 March 1978" 
+};
+
+const Panama = {
+    code: "PA",
+    name: "Panama",
+    joined: "7 September 2012" 
+};
+
