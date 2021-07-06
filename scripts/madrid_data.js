45dde1f6164a971b686841bf30a1438064da3fee
AntonHuggard <antonhuggard@gmail.com>
2021-07-07 08:32:38 +1200

    Init Madrid code

---
+++ scripts/madrid_data.js
@@ -0,0 +1,432 @@
+Afghanistan = {
+    madrid_agreement: "",
+    madrid_protocol: "June 26, 2018"
+};
+African Intellectual Property Organization (OAPI) = {
+    madrid_agreement: "",
+    madrid_protocol: "March 5, 2015"
+};
+Albania  = {
+    madrid_agreement: "October 4, 1995",
+    madrid_protocol: "July 30, 2003"
+};
+Algeria  = {
+    madrid_agreement: "July 5, 1972",
+    madrid_protocol: "October 31, 20155"
+};
+Antigua and Barbuda  = {
+    madrid_agreement: "",
+    madrid_protocol: "March 17, 2000"
+};
+Armenia  = {
+    madrid_agreement: "December 25, 1991",
+    madrid_protocol: "October 19, 2000"
+};
+Australia  = {
+    madrid_agreement: "",
+    madrid_protocol: "July 11, 2001"
+};
+Austria  = {
+    madrid_agreement: "January 1, 1909",
+    madrid_protocol: "April 13, 1999"
+};
+Azerbaijan  = {
+    madrid_agreement: "December 25, 1995",
+    madrid_protocol: "April 15, 2007"
+};
+Bahrain  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 15, 2005"
+};
+Belarus = {
+    madrid_agreement: "December 25, 1991",
+    madrid_protocol: "January 18, 2002"
+};
+Belgium  = {
+    madrid_agreement: "July 15, 1892",
+    madrid_protocol: "April 1, 1998"
+};
+Bhutan  = {
+    madrid_agreement: "August 4, 2000",
+    madrid_protocol: "August 4, 2000" 
+};
+Bosnia and Herzegovina  = {
+    madrid_agreement: "March 1, 1992",
+    madrid_protocol: "January 27, 2009"
+};
+Botswana  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 5, 2006"
+};
+Brazil  = {
+    madrid_agreement: "",
+    madrid_protocol: "October 2, 2019"
+};
+Brunei Darussalam  = {
+    madrid_agreement: "",
+    madrid_protocol: "January 6, 2017"
+};
+Bulgaria  = {
+    madrid_agreement: "August 1, 1985",
+    madrid_protocol: "October 2, 2001"
+};
+Cambodia = {
+    madrid_agreement: "",
+    madrid_protocol: "June 5, 2015"
+};
+Canada  = {
+    madrid_agreement: "",
+    madrid_protocol: "June 17, 2019"
+};
+China  = {
+    madrid_agreement: "October 4, 1989",
+    madrid_protocol: "December 1, 1995"
+};
+Colombia  = {
+    madrid_agreement: "",
+    madrid_protocol: "August 29, 2012"
+};
+Croatia  = {
+    madrid_agreement: "October 8, 1991",
+    madrid_protocol: "January 23, 2004"
+};
+Cuba = {
+    madrid_agreement: "December 6, 1989",
+    madrid_protocol: "December 26, 1995"
+};
+Cyprus  = {
+    madrid_agreement: "November 4, 2003",
+    madrid_protocol: " November 4, 2003"
+};
+Czech Republic = {
+    madrid_agreement: "January 1, 1993",
+    madrid_protocol: "September 25, 1996"
+};
+Democratic People's Republic of Korea  = {
+    madrid_agreement: "June 10, 1980",
+    madrid_protocol: "October 3, 1996"
+};
+Denmark  = {
+    madrid_agreement: "",
+    madrid_protocol: "February 13, 1996"
+};
+Egypt  = {
+    madrid_agreement: "",
+    madrid_protocol: "July 1, 1952 September 3, 2009"
+};
+Estonia  = {
+    madrid_agreement: "",
+    madrid_protocol: "November 18, 1998,8"
+};
+Eswatini = {
+    madrid_agreement: "",
+    madrid_protocol: "December 14, 1998 December 14, 1998"
+};
+European Union = {
+    madrid_agreement: "",
+    madrid_protocol: "October 1, 2004"
+};
+Finland = {
+    madrid_agreement: "",
+    madrid_protocol: "April 1, 1996"
+};
+France  = {
+    madrid_agreement: "July 15, 18929",
+    madrid_protocol: "November 7, 19979"
+};
+Gambia  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 18, 2015"
+};
+Georgia  = {
+    madrid_agreement: "",
+    madrid_protocol: "August 20, 1998"
+};
+Germany  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 1, 1922 March 20, 1996"
+};
+Ghana = {
+    madrid_agreement: "September 16, "",
+    madrid_protocol: 008"
+};
+Greece = {
+    madrid_agreement: "",
+    madrid_protocol: "August 10, 2000"
+};
+Hungary  = {
+    madrid_agreement: "",
+    madrid_protocol: "January 1, 1909 October 3, 1997"
+};
+Iceland  = {
+    madrid_agreement: "",
+    madrid_protocol: "April 15, 1997"
+};
+India = {
+    madrid_agreement: "",
+    madrid_protocol: "July 8, 2013,8"
+};
+Indonesia  = {
+    madrid_agreement: "January 2, 20",
+    madrid_protocol: "8"
+};
+Iran (Islamic Republic of) = {
+    madrid_agreement: "",
+    madrid_protocol: "December 25, 2003 December 25, 20035"
+};
+Ireland = {
+    madrid_agreement: "October 19, 20",
+    madrid_protocol: "1"
+};
+Israel  = {
+    madrid_agreement: "",
+    madrid_protocol: "September 1, 2010"
+};
+Italy = {
+    madrid_agreement: "",
+    madrid_protocol: "October 15, 1894 April 17, 2000"
+};
+Japan = {
+    madrid_agreement: "",
+    madrid_protocol: "March 14, 2000"
+};
+Kazakhstan  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 25, 1991 December 8, 2010"
+};
+Kenya  = {
+    madrid_agreement: "June 26, 1998",
+    madrid_protocol: "June 26, 1998"
+};
+Kyrgyzstan = {
+    madrid_agreement: "",
+    madrid_protocol: "December 25, 1991 June 17, 20046"
+};
+Lao People’s Democratic Republic  = {
+    madrid_agreement: "",
+    madrid_protocol: "March 7, 2016"
+};
+Latvia = {
+    madrid_agreement: "",
+    madrid_protocol: "January 1, 1995 January 5, 2000"
+};
+Lesotho  = {
+    madrid_agreement: "February 12,"",
+    madrid_protocol: 1999 February 12, 1999"
+};
+Liberia  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 25, 1995 December 11, 2009"
+};
+Liechtenstein = {
+    madrid_agreement: "",
+    madrid_protocol: "July 14, 1933 March 17, 1998"
+};
+Lithuania = {
+    madrid_agreement: "",
+    madrid_protocol: "November 15, 19975"
+};
+Luxembourg  = {
+    madrid_agreement: "",
+    madrid_protocol: "September 1, 19243 April 1, 1998"
+};
+Madagascar = {
+    madrid_agreement: "",
+    madrid_protocol: "April 28, 2008"
+};
+Malawi = {
+    madrid_agreement: "",
+    madrid_protocol: "December 25, 2018"
+};
+Malaysia  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 27, 2019"
+};
+Mexico = {
+    madrid_agreement: "",
+    madrid_protocol: "February 19, 2013"
+};
+Monaco = {
+    madrid_agreement: "April 29, 1956",
+    madrid_protocol: "September 27, 1996"
+};
+Mongolia  = {
+    madrid_agreement: "April 21, 1985",
+    madrid_protocol: "June 16, 2001"
+};
+Montenegro = {
+    madrid_agreement: "June 3, 2006",
+    madrid_protocol: "June 3, 2006"
+};
+Morocco = {
+    madrid_agreement: "July 30, 1917",
+    madrid_protocol: "October 8, 1999"
+};
+Mozambique  = {
+    madrid_agreement: "October 7, 1998",
+    madrid_protocol: "October 7, 1998"
+};
+Namibia  = {
+    madrid_agreement: "June 30, 2004",
+    madrid_protocol: "June 30, 20048"
+};
+Netherlands = {
+    madrid_agreement: "March 1, 1893",
+    madrid_protocol: "April 1, 1998"
+};
+New Zealand = {
+    madrid_agreement: "",
+    madrid_protocol: "December 10, 2012"
+};
+North Macedonia  = {
+    madrid_agreement: "September 8, 1991",
+    madrid_protocol: "Augst 30, 2002"
+};
+Norway  = {
+    madrid_agreement: "",
+    madrid_protocol: "March 29, 1996"
+};
+Oman  = {
+    madrid_agreement: "",
+    madrid_protocol: "October 16, 2007"
+};
+Pakistan  = {
+    madrid_agreement: "",
+    madrid_protocol: "May 24, 2021"
+};
+Philippines = {
+    madrid_agreement: "",
+    madrid_protocol: "July 25, 2012"
+};
+Poland = {
+    madrid_agreement: "March 18, 1991",
+    madrid_protocol: "March 4, 1997"
+};
+Portugal  = {
+    madrid_agreement: "October 31, 1893",
+    madrid_protocol: "March 20, 1997"
+};
+Republic of Korea = {
+    madrid_agreement: "",
+    madrid_protocol: "April 10, 2003"
+};
+Republic of Moldova  = {
+    madrid_agreement: "December 25, 1991",
+    madrid_protocol: "December 1, 1997"
+};
+Romania = {
+    madrid_agreement: "October 6, 1920",
+    madrid_protocol: "July 28, 1998"
+};
+Russian Federation  = {
+    madrid_agreement: "July 1, 1976",
+    madrid_protocol: "June 10, 1997"
+};
+Rwanda = {
+    madrid_agreement: "",
+    madrid_protocol: "August 17, 2013"
+};
+Samoa  = {
+    madrid_agreement: "",
+    madrid_protocol: "March 4, 2019"
+};
+San Marino  = {
+    madrid_agreement: "September 25, 1960",
+    madrid_protocol: "September 12, 2007"
+};
+Sao Tome and Principe = {
+    madrid_agreement: "",
+    madrid_protocol: "December 8, 2008"
+};
+Serbia14  = {
+    madrid_agreement: "April 27, 1992",
+    madrid_protocol: "February 17, 1998"
+};
+Sierra Leone = {
+    madrid_agreement: "June 17, 1997",
+    madrid_protocol: "December 28, 1999"
+};
+Singapore = {
+    madrid_agreement: "",
+    madrid_protocol: "October 31, 2000"
+};
+Slovakia  = {
+    madrid_agreement: "January 1, 1993",
+    madrid_protocol: "September 13, 1997"
+};
+Slovenia  = {
+    madrid_agreement: "June 25, 1991",
+    madrid_protocol: "March 12, 1998"
+};
+Spain = {
+    madrid_agreement: "July 15, 1892",
+    madrid_protocol: "December 1, 1995"
+};
+Sudan = {
+    madrid_agreement: "May 16, 1984",
+    madrid_protocol: "February 16, 2010"
+};
+Sweden  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 1, 1995"
+};
+Switzerland = {
+    madrid_agreement: "July 15, 1892",
+    madrid_protocol: "May 1, 1997"
+};
+Syrian Arab Republic  = {
+    madrid_agreement: "",
+    madrid_protocol: "August 5, 2004"
+};
+Tajikistan  = {
+    madrid_agreement: "December 25, 1991",
+    madrid_protocol: "June 3, 2011"
+};
+Thailand = {
+    madrid_agreement: "",
+    madrid_protocol: "November 7, 2017"
+};
+Trinidad and Tobago  = {
+    madrid_agreement: "",
+    madrid_protocol: "January 12, 2021"
+};
+Tunisia  = {
+    madrid_agreement: "",
+    madrid_protocol: "October 16, 2013"
+};
+Turkey  = {
+    madrid_agreement: "",
+    madrid_protocol: "January 1, 1999"
+};
+Turkmenistan = {
+    madrid_agreement: "",
+    madrid_protocol: "September 28, 1999"
+};
+Ukraine  = {
+    madrid_agreement: "December 25, 1991",
+    madrid_protocol: "December 29, 2000"
+};
+United Kingdom  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 1, 1995"
+};
+United States of America = {
+    madrid_agreement: "",
+    madrid_protocol: "November 2, 2003"
+};
+Uzbekistan  = {
+    madrid_agreement: "",
+    madrid_protocol: "December 27, 2006"
+};
+Viet Nam  = {
+    madrid_agreement: "March 8, 1949",
+    madrid_protocol: " July 11, 2006"
+};
+Zambia = {
+    madrid_agreement: "",
+    madrid_protocol: "November 15, 2006"
+};
+Zimbabwe  = {
+    madrid_agreement: "",
+    madrid_protocol: "March 11, 2015"
+};
