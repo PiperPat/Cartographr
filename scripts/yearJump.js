bc9867c813512877a432a7b99ccac7ab4ff80ed5
AntonHuggard <antonhuggard@gmail.com>
2021-10-01 17:22:08 +1300

    Fixed flashing map bug

--- scripts/yearJump.js
+++ scripts/yearJump.js
@@ -1,8 +1,10 @@
 
 function yearJump() {
     var jump_to = document.getElementById('year-input').value;
-    let year = parseInt(jump_to, 10); // must be converted from str to int
+    let year = parseInt(jump_to, 10); // must be converted from str to int (in base 10)
     if (! isNaN(year)) {
+        if (year < 1978) year = 1978;
+        if (year > 2020) year = 2020;
         currentYear = year;
         changeYear(year);
     }
